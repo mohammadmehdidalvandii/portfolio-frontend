1c3b7a842c7ac73ca63935783e3fa7f44f8c2e90
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-11-27 10:46:16 +0330

    created section Projects for home-page

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,10 +1,12 @@
 import Hero from '@components/template/home/Hero/Hero'
+import Projects from '@components/template/home/Projects/Projects'
 import React from 'react'
 
 const Home:React.FC = ()=>{
   return (
     <>
     <Hero/>
+    <Projects/>
     </>
   )
 }
