b426581bcf65aae24c1bbc101425ee82f4a856e9
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-11-29 15:52:25 +0330

    created layoutAdmin or admin-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import AdminLayout from "@layouts/AdminLayout/AdminLayout";
+import Admin from "@page/Admin/Admin";
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -12,6 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/Login" element={<Login/>}/>
+        <Route path="/Admin" element={<AdminLayout/>}>
+          <Route index element={<Admin/>} />
+        </Route>
       </Routes>
     </Suspense>
     </BrowserRouter>
