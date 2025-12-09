fff6f5e3116e59d7867931c5efe8dec068cc07cd
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-09 16:11:54 +0330

    show skills admin panels

--- src/components/template/Admin/SkillsAdmin/SkillsAdmin.tsx
+++ src/components/template/Admin/SkillsAdmin/SkillsAdmin.tsx
@@ -1,53 +1,71 @@
-import { Edit2, Plus, Trash2 } from 'lucide-react'
-import React, { useState } from 'react'
-import AddSkill from './AddSkill';
-import EditSkill from './EditSkill';
+import { Edit2, Plus, Trash2 } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import AddSkill from "./AddSkill";
+import EditSkill from "./EditSkill";
+import { SkillProps } from "../../../../types/skills";
 
-const SkillsAdmin:React.FC = ()=>{
-    const [isAddSkill , setIsAddSkill] = useState(false);
-    const [isEditSkill , setIsEditSkill] = useState(false);
+const SkillsAdmin: React.FC = () => {
+  const [isAddSkill, setIsAddSkill] = useState(false);
+  const [isEditSkill, setIsEditSkill] = useState(false);
+  const [skills, setSkills] = useState<SkillProps[]>([]);
 
-
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:3000/api/skills");
+      if (res.ok) {
+        const data = await res.json();
+        setSkills(data.data);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="space-y-4">
-        {!isAddSkill && ( 
-         <button className="admin_btn flex items-center justify-center gap-2"
-        onClick={()=>setIsAddSkill(true)}
+      {!isAddSkill && (
+        <button
+          className="admin_btn flex items-center justify-center gap-2"
+          onClick={() => setIsAddSkill(true)}
         >
-            <Plus className='w-5 h-5'/>
-            Add Skill
-        </button>)}
-        {isAddSkill && (<AddSkill
-        handlerAddSkillCancel={()=>setIsAddSkill(false)}
-        />)}
+          <Plus className="w-5 h-5" />
+          Add Skill
+        </button>
+      )}
+      {isAddSkill && (
+        <AddSkill handlerAddSkillCancel={() => setIsAddSkill(false)} />
+      )}
 
-        <div className="space-y-3">
-            {!isEditSkill && (
-            <div className="bg-slate-800/50 p-4 rounded-lg">
-                <div className="flex justify-between items-center">
-                    <div className="flex-1">
-                        <h3 className="font-InterBold font-semibold text-white">Javascript</h3>
-                    </div>
-                    <div className="flex gap-2 ml-4">
-                        <button className="p-2 text-blue-400 hover:bg-slate-700 rounded cursor-pointer"
-                        onClick={()=>setIsEditSkill(true)}
-                        >
-                            <Edit2 className='w-4 h-4'/>
-                        </button>
-                        <button className="p-2 text-red-400 hover:bg-slate-700 rounded cursor-pointer">
-                            <Trash2 className='w-4 h-4'/>
-                        </button>
-                    </div>
-                </div>
+      <div className="space-y-3">
+        {!isEditSkill && (
+            skills.map((skill)=>(
+          <div className="bg-slate-800/50 p-4 rounded-lg" key={skill._id}>
+            <div className="flex justify-between items-center">
+              <div className="flex-1">
+                <h3 className="font-InterBold font-semibold text-white">
+                  {skill.name}
+                </h3>
+              </div>
+              <div className="flex gap-2 ml-4">
+                <button
+                  className="p-2 text-blue-400 hover:bg-slate-700 rounded cursor-pointer"
+                  onClick={() => setIsEditSkill(true)}
+                >
+                  <Edit2 className="w-4 h-4" />
+                </button>
+                <button className="p-2 text-red-400 hover:bg-slate-700 rounded cursor-pointer">
+                  <Trash2 className="w-4 h-4" />
+                </button>
+              </div>
             </div>
-            )}
-        </div>
-        {isEditSkill && (<EditSkill
-        handlerEditSkillCancel={()=>setIsEditSkill(false)}
-        />)}
+          </div>
+            ))
+        )}
+      </div>
+      {isEditSkill && (
+        <EditSkill handlerEditSkillCancel={() => setIsEditSkill(false)} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default SkillsAdmin
+export default SkillsAdmin;
